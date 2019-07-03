061969265570b61c8b411b58b5c05c9489ca3da5
sarathkumarreddykaluva <52447697+sarathkumarreddykaluva@users.noreply.github.com>
2019-07-03 09:44:28 +0530

    Update vow.py

--- vow.py
+++ vow.py
@@ -1,7 +1,9 @@
-n=(input())
-if( n=='a' or n=='e'or n=='i' or   n=='o' or n=='u'):
-    print(n,"Vowel")
-elif( n!='a' or n!='e'or n!='i' or  n!='o' or n!='u'):
-    print(n, "Consonant")
+a=str(input())
+list1=["a","e","i","o","u"]
+list2=["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","y","z"]
+if a in list1:
+    print("Vowel")
+elif a in list2:
+    print("Consonant")
 else:
-    print("invalid")
+    print("Invalid")
