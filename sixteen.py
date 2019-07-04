ad65eb6ff234e758e8896febbee1abf947fa0bd0
sarathkumarreddykaluva <52447697+sarathkumarreddykaluva@users.noreply.github.com>
2019-07-04 11:17:49 +0530

    Create sixteen.py

---
+++ sixteen.py
@@ -0,0 +1,8 @@
+lo,up=map(int,input().split())
+for n in range(lo,up + 1):
+   if n > 1:
+       for i in range(2,n):
+           if (n % i) == 0:
+               break
+       else:
+           print(n,end=' ')
