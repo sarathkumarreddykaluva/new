f296e20030186084106ccb84e8f66f9fc262d229
sarathkumarreddykaluva <52447697+sarathkumarreddykaluva@users.noreply.github.com>
2019-07-11 15:44:16 +0530

    Create hunter2.py

---
+++ hunter2.py
@@ -0,0 +1,12 @@
+p=list(map(str,input()))
+v=t=0
+for i in range(0,len(p)-1):
+  q=p[i]
+  if int(q)!=0:
+   for j in range(i+1,i+2):
+    q=q+p[j]
+    if int(q)<27 and int(q)>0: v=v+1
+    elif int(q)==0: v=v-1
+    else: break
+if v!=1: t=v%2
+print(v+t+1)
